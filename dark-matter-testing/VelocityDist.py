b49694cb264b1f4790fb19a648b9b572499810f8
JeppJeppsson <jepsalt@gmail.com>
2022-04-24 19:00:20 +0200

    syntax cleanup

--- dark-matter-testing/VelocityDist.py
+++ dark-matter-testing/VelocityDist.py
@@ -19,7 +19,7 @@ def p1(v_z):
         return (a*(math.erf((v_e-v_z)/v_0) + math.erf((v_e+v_z)/v_0) - \
                 (np.pi*v_0**2/N_0)*np.exp(-v_esc**2/v_0**2)))*(v_z < v_esc - v_e) + \
                 (a*(math.erf(v_esc/v_0) + math.erf((v_e-v_z)/v_0)) - \
-                (np.pi*v_0**2)/(2*N_0)*(v_e + v_esc - v_z)/v_e*np.exp(-v_esc**2/v_0**2)) *\
+                (np.pi*v_0**2)/(2*N_0)*(v_e + v_esc - v_z)/v_e*np.exp(-v_esc**2/v_0**2)) * \
                 (v_esc - v_e < v_z < v_esc + v_e)
 
 m_chi = 1e4
@@ -35,7 +35,7 @@ contour_func = np.zeros((len(K), len(K[0])))
 
 for i in range(len(K)):
         for j in range(len(K[0])):
-                contour_func[i][j] = W[i][j]/K[i][j]*p1(W[i][j]/K[i][j] +K[i][j]/(2*m_chi))
+                contour_func[i][j] = W[i][j]/K[i][j]*p1(W[i][j]/K[i][j] + K[i][j]/(2*m_chi))
                 X[i][j] =  K[i][j]/(v_0*m_chi)
                 Y[i][j] = W[i][j]/(v_0**2*m_chi) 
 
